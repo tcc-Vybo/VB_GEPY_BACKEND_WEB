d8b7b3b77808ac39d235666544b8de20c460ce6a
Gustavo Henrique <gustavosilvah37@gmail.com>
2024-09-02 19:22:10 -0300

    feat: project created

---
+++ src/main/java/gepy/vybo/tcc/VB_GEPY_BACKEND_WEB/VbGepyBackendWebApplication.java
@@ -0,0 +1,13 @@
+package gepy.vybo.tcc.VB_GEPY_BACKEND_WEB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VbGepyBackendWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VbGepyBackendWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gepy/vybo/tcc/VB_GEPY_BACKEND_WEB/VbGepyBackendWebApplicationTests.java
@@ -0,0 +1,13 @@
+package gepy.vybo.tcc.VB_GEPY_BACKEND_WEB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VbGepyBackendWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

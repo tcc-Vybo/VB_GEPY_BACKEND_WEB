a2f07856d0a89cab01e32ed681bc3ac7a4641e97
unknown <gpereira383@gmail.com>
2024-09-09 19:50:32 -0300

    feat: aluno's entity and dto classes completed

--- src/main/java/gepy/vybo/tcc/VB_GEPY_BACKEND_WEB/entity/AlunoEntity.java
+++ src/main/java/gepy/vybo/tcc/VB_GEPY_BACKEND_WEB/entity/AlunoEntity.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.beans.BeanUtils;
 
+import java.util.Date;
+
 @Entity
 @Table (name = "aluno")
 @Getter
@@ -19,6 +21,82 @@ public class AlunoEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private  Long id;
+
+    // Identificação
+    @Column(nullable = false)
+    private String nomeCompleto;
+
+    @Column(nullable = false)
+    private Date dataNascimento;
+
+    @Column(nullable = false)
+    private String cidadeNascimento;
+
+    @Column(nullable = false)
+    private String ufNascimento;
+
+    @Column(nullable = false)
+    private String nacionalidade;
+
+    @Column(nullable = false)
+    private String genero;
+
+    @Column(nullable = false)
+    private String corRaca;
+
+    @Column(nullable = false)
+    private String necessidades;
+
+    // Endereço
+    @Column(nullable = false)
+    private int cep;
+
+    @Column(nullable = false)
+    private String endereco;
+
+    @Column(nullable = false)
+    private int numeroEndereco;
+
+    private String complemento;
+
+    @Column(nullable = false)
+    private String bairro;
+
+    @Column(nullable = false)
+    private String municipio;
+
+    @Column(nullable = false)
+    private String uf;
+
+    // RG
+    @Column(nullable = false)
+    private String numeroRegistro;
+
+    @Column(nullable = false)
+    private String cpf;
+
+    @Column(nullable = false)
+    private String dataEmissao;
+
+    @Column(nullable = false)
+    private String orgaoExpedidor;
+
+    // Contato Aluno
+    @Column(nullable = false)
+    private String telefoneAluno;
+
+    // Contato Responsáveis
+    @Column(nullable = false)
+    private String nomeResponsavel;
+
+    @Column(nullable = false)
+    private String cpfResponsavel;
+
+    @Column(nullable = false)
+    private String relacao;
+
+    @Column(nullable = false)
+    private String telefoneResponsavel;
 
     public AlunoEntity(AlunoDTO aluno) {
         BeanUtils.copyProperties(aluno, this);

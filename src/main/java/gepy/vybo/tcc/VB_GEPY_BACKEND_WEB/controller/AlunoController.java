795e4cf2c72496a787eb3ef8d8a94e80f58dfa04
Emily Leal <emily.passos18@outlook.com>
2024-09-10 20:01:19 -0300

    Controller e Service completos

--- src/main/java/gepy/vybo/tcc/VB_GEPY_BACKEND_WEB/controller/AlunoController.java
+++ src/main/java/gepy/vybo/tcc/VB_GEPY_BACKEND_WEB/controller/AlunoController.java
@@ -1,4 +1,41 @@
 package gepy.vybo.tcc.VB_GEPY_BACKEND_WEB.controller;
 
+import gepy.vybo.tcc.VB_GEPY_BACKEND_WEB.dto.AlunoDTO;
+import gepy.vybo.tcc.VB_GEPY_BACKEND_WEB.service.AlunoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/aluno")
+@CrossOrigin
 public class AlunoController {
-}
+
+    @Autowired
+    private AlunoService alunoService;
+
+    @GetMapping
+    public List<AlunoDTO> ListarTodos(){
+        return alunoService.ListarTodos();
+    }
+
+    @PostMapping
+    public void inserir(@RequestBody AlunoDTO aluno){
+        alunoService.inserir(aluno);
+    }
+
+    @PutMapping
+    public AlunoDTO alterar (@RequestBody AlunoDTO aluno){
+        return alunoService.alterar(aluno);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluir(@PathVariable("id") Long id){
+        alunoService.excluir(id);
+        return ResponseEntity.ok().build();
+    }
+
+
+};

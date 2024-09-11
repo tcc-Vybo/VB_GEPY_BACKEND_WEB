8725cf2f3d7d3fef283e0c7faa9e03dd408909e9
Guilherme Rosa <147450461+guipereirar@users.noreply.github.com>
2024-09-11 19:16:33 -0300

    feat: funcionario CRUD created

---
+++ src/main/java/gepy/vybo/tcc/VB_GEPY_BACKEND_WEB/dto/FuncionarioDTO.java
@@ -0,0 +1,11 @@
+package gepy.vybo.tcc.VB_GEPY_BACKEND_WEB.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class FuncionarioDTO {
+}

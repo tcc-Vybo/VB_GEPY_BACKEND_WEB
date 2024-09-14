4861a558e3ab4b02e3958e84088bb8d366a5110f
Emily Leal <emily.passos18@outlook.com>
2024-09-13 21:20:03 -0300

    Controller e Service funcionario completo

--- src/main/java/gepy/vybo/tcc/VB_GEPY_BACKEND_WEB/controller/FuncionarioController.java
+++ src/main/java/gepy/vybo/tcc/VB_GEPY_BACKEND_WEB/controller/FuncionarioController.java
@@ -1,11 +1,54 @@
 package gepy.vybo.tcc.VB_GEPY_BACKEND_WEB.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import gepy.vybo.tcc.VB_GEPY_BACKEND_WEB.dto.FuncionarioDTO;
+import gepy.vybo.tcc.VB_GEPY_BACKEND_WEB.service.FuncionarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/funcionario")
 @CrossOrigin
 public class FuncionarioController {
+
+    @Autowired
+    private FuncionarioService funcionarioService;
+
+    @GetMapping
+    public  List<FuncionarioDTO> ListarTodos(){
+        return funcionarioService.ListarTodos();
+    }
+
+    @PostMapping
+    public void inserir(@RequestBody FuncionarioDTO funcionario){
+        funcionarioService.inserir(funcionario);
+    }
+
+    @PutMapping
+    public FuncionarioDTO alterar (@RequestBody FuncionarioDTO funcionario){
+        return funcionarioService.alterar(funcionario);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluir(@PathVariable("id") Long id){
+        funcionarioService.excluir(id);
+        return ResponseEntity.ok().build();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/main/java/gepy/vybo/tcc/VB_GEPY_BACKEND_WEB/service/FuncionarioService.java
+++ src/main/java/gepy/vybo/tcc/VB_GEPY_BACKEND_WEB/service/FuncionarioService.java
@@ -1,7 +1,82 @@
 package gepy.vybo.tcc.VB_GEPY_BACKEND_WEB.service;
 
+import gepy.vybo.tcc.VB_GEPY_BACKEND_WEB.dto.FuncionarioDTO;
+import gepy.vybo.tcc.VB_GEPY_BACKEND_WEB.entity.FuncionarioEntity;
+import gepy.vybo.tcc.VB_GEPY_BACKEND_WEB.repository.FuncionarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FuncionarioService {
+
+    @Autowired
+    private FuncionarioRepository funcionarioRepository;
+
+    public List<FuncionarioDTO> ListarTodos(){
+        List<FuncionarioEntity> funcionarios = funcionarioRepository.findAll();
+        return funcionarios.stream().map(FuncionarioDTO::new).toList();
+    }
+
+    public void inserir(FuncionarioDTO funcionario){
+        FuncionarioEntity funcionarioEntity = new FuncionarioEntity(funcionario);
+        funcionarioRepository.save(funcionarioEntity);
+    }
+
+    public FuncionarioDTO alterar(FuncionarioDTO funcionario){
+        FuncionarioEntity funcionarioEntity = new FuncionarioEntity(funcionario);
+        return new FuncionarioDTO(funcionarioRepository.save(funcionarioEntity));
+    }
+
+    public void excluir(Long id){
+        FuncionarioEntity funcionario= funcionarioRepository.findById(id).get();
+        funcionarioRepository.delete(funcionario);
+    }
+
+    public FuncionarioDTO buscarPorId(Long id){
+        return new FuncionarioDTO((funcionarioRepository.findById(id).get()));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }

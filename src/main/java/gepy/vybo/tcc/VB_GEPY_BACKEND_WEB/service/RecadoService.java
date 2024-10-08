07dc0ebce009e0ccb562ec86207d805e34d2e4e1
Gustavo Henrique <gustavosilvah37@gmail.com>
2024-10-08 10:26:01 -0300

    feat: testing response with message

--- src/main/java/gepy/vybo/tcc/VB_GEPY_BACKEND_WEB/service/RecadoService.java
+++ src/main/java/gepy/vybo/tcc/VB_GEPY_BACKEND_WEB/service/RecadoService.java
@@ -4,9 +4,13 @@ import gepy.vybo.tcc.VB_GEPY_BACKEND_WEB.dto.RecadoDTO;
 import gepy.vybo.tcc.VB_GEPY_BACKEND_WEB.entity.RecadoEntity;
 import gepy.vybo.tcc.VB_GEPY_BACKEND_WEB.repository.RecadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class RecadoService {
@@ -19,9 +23,22 @@ public class RecadoService {
         return recado.stream().map(RecadoDTO::new).toList();
     }
 
-    public void inserir(RecadoDTO recado){
+    public ResponseEntity<Map<String, String>> inserir(RecadoDTO recado){
         RecadoEntity recadoEntity = new RecadoEntity(recado);
-        recadoRepository.save(recadoEntity);
+
+        Map<String, String> response = new HashMap<>();
+
+        try {
+            recadoRepository.save(recadoEntity);
+            response.put("message", "Evento inserido com sucesso!!");
+            return ResponseEntity.ok(response);
+
+        }catch(Exception e){
+            response.put("error", "Evento não inserido!!");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
+
+
     }
 
     public RecadoDTO alterar(RecadoDTO recado){

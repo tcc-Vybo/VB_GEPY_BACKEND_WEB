0597c8842df793799dbabf2aafe43af75f3426f2
Gustavo Henrique <gustavosilvah37@gmail.com>
2024-11-16 00:11:05 -0300

    fix: testing

--- src/main/java/gepy/vybo/tcc/VB_GEPY_BACKEND_WEB/repository/BoletimRepository.java
+++ src/main/java/gepy/vybo/tcc/VB_GEPY_BACKEND_WEB/repository/BoletimRepository.java
@@ -3,6 +3,7 @@ package gepy.vybo.tcc.VB_GEPY_BACKEND_WEB.repository;
 import gepy.vybo.tcc.VB_GEPY_BACKEND_WEB.entity.BoletimEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -14,6 +15,6 @@ public interface BoletimRepository extends JpaRepository<BoletimEntity, Long> {
     @Query(value = "SELECT b.* FROM boletim b WHERE b.id_disciplina = ?", nativeQuery = true)
     List<BoletimEntity> findAllByDisciplina(Long idDisciplina);
 
-    @Query(value = "SELECT b.* FROM boletim b WHERE b.id_aluno = ?", nativeQuery = true)
-    List<BoletimEntity> findAllByAluno(Long idAluno);
+    @Query(value = "SELECT b.* FROM boletim b WHERE b.id_aluno = :idAluno", nativeQuery = true)
+    List<BoletimEntity> findAllByAluno(@Param("idAluno") Long idAluno);
 }

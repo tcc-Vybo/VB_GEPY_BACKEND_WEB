c06d2fec3369ce90c276973a3f348e9796a8111b
Gustavo Henrique <gustavosilvah37@gmail.com>
2024-12-04 20:35:41 -0300

    feat: testing

--- src/main/java/gepy/vybo/tcc/VB_GEPY_BACKEND_WEB/service/PerfilVinculoService.java
+++ src/main/java/gepy/vybo/tcc/VB_GEPY_BACKEND_WEB/service/PerfilVinculoService.java
@@ -9,11 +9,13 @@ import gepy.vybo.tcc.VB_GEPY_BACKEND_WEB.repository.TurmaDisciplinaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Service
 public class PerfilVinculoService {
 
     @Autowired
